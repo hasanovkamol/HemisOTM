b8a1b4aeaefcc5630167a5e2d772bdfa9ec484ab
KamolHasanov <khasanovkamol3834@gmail.com>
2021-05-24 22:52:49 +0500

    change code direaction

--- HemisOTM/Controllers/DirectionsController.cs
+++ HemisOTM/Controllers/DirectionsController.cs
@@ -18,29 +18,7 @@ namespace HemisOTM.Controllers
             _context = context;
         }
 
-        // GET: Directions
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Directions.ToListAsync());
-        }
-
-        // GET: Directions/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var direction = await _context.Directions
-                .FirstOrDefaultAsync(m => m.DirectionId == id);
-            if (direction == null)
-            {
-                return NotFound();
-            }
-
-            return View(direction);
-        }
+     
 
         public IActionResult Create()
         {
@@ -62,8 +40,6 @@ namespace HemisOTM.Controllers
             ViewBag.direction = directions;
             return View();
         }
-
-        // GET: Directions/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -79,9 +55,6 @@ namespace HemisOTM.Controllers
             return View(direction);
         }
 
-        // POST: Directions/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("DirectionId,Code,Name")] Direction direction)
@@ -114,8 +87,7 @@ namespace HemisOTM.Controllers
             return View(direction);
         }
 
-        // GET: Directions/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
@@ -132,7 +104,6 @@ namespace HemisOTM.Controllers
             return View(direction);
         }
 
-        // POST: Directions/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
