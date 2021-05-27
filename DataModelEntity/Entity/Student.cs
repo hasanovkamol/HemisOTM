e42d87a81009f7306037711a09ce3267b1c6ee3d
KamolHasanov <khasanovkamol3834@gmail.com>
2021-05-27 09:51:45 +0500

    chnage model for student

--- DataModelEntity/Entity/Student.cs
+++ DataModelEntity/Entity/Student.cs
@@ -10,14 +10,18 @@ namespace DataModelEntity.Entity
     {
         [Key]
         public int StudentId { get; set; }
+        [Display(Name = "Ism")]
         public string Name { get; set; }
+        [Display(Name = "Familya")]
         public string Surname { get; set; }
-        [Display(Name="Middil name")]
+        [Display(Name= "Otasini ismi")]
         public string Middilname { get; set; }
-        [Display(Name="Diraction name")]
+        [Display(Name="Yo'nalish")]
         public Direction GetDirection { get; set; }
         public int DirectionId { get; set; }
+        [Display(Name = "Kurs")]
         public int Course { get; set; }
+        [Display(Name = "Birlashgan gruh nomi")]
         public string GrupName { get; set; }
        
 
